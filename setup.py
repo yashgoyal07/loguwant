5cb212d74e77eaae8e3bda5079469ac034a9aeeb
Yash Goyal <yashgoyal@Yashs-MacBook-Pro.local>
2022-03-07 00:58:49 +0530

    final package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="loguwant",
-    version="0.0.2",
+    version="0.0.3",
     author="Yash Goyal",
     author_email="yashgoyalcs@gmail.com",
     long_description=long_description,
